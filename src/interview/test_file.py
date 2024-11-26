c44abd908ef359e2809025ded11d2cedf27cca16
DESKTOP-FP4OP26\Tobias <tobias-westmeier@t-online.de>
2024-11-26 13:26:45 +0100

    New file: src/interview/test_file.py

---
+++ src/interview/test_file.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pytest
+
+def add_vars(var1, var2):
+    return var1 + var2
---
+++ tests/test_add.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_add_vars_integers():
+    assert add_vars(1, 2) == 3
+
+def test_add_vars_floats():
+    assert add_vars(1.5, 2.5) == 4.0
+
+def test_add_vars_arrays():
+    assert np.array_equal(add_vars(np.array([1, 2]), np.array([3, 4])), np.array([4, 6]))
+
+def test_add_vars_strings():
+    assert add_vars("Hello, ", "world!") == "Hello, world!"
